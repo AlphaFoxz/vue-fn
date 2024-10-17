10bb57e33387c66777c8b643330e76f0d51af1a0
AlphaFoxz <841958335@qq.com>
2024-10-17 20:22:35 +0800

    remove dist

--- dist/timer/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { type Ref } from '@vue/reactivity';
-export interface TimeoutApi {
-    resolve: Ref<() => void>;
-    reject: Ref<(error: Error) => void>;
-    reset: (ms?: number) => void;
-    promise: Promise<void>;
-}
-export declare function createTimeout(timeoutMs: number, timeoutError?: Error): TimeoutApi;
